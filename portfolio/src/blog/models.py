e97d38c59374ed68f2c1ec69e5eeea0f6bc78c3f
Ashraful Islam Alvee <alvmz32@gmail.com>
2020-01-06 07:48:44 +0600

    created blog model field

--- portfolio/src/blog/models.py
+++ portfolio/src/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+#1 Create your models here.
+# title, pub_date, body, image
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
